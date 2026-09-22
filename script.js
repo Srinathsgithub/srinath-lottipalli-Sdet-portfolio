7d034b192f6330585022ea618dad4d1186594269
Srinathsgithub <153265075+Srinathsgithub@users.noreply.github.com>
2026-09-23 00:57:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu");
-const nav = document.querySelector(".nav-links");
-menu?.addEventListener("click", () => nav.classList.toggle("open"));
-document.querySelectorAll(".nav-links a").forEach(a => {
-  a.addEventListener("click", () => nav.classList.remove("open"));
-});
